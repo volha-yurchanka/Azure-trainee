7e6515525d2b5fd31e6b3f16ca49c5090ebfdbc8
Yurchanka-Volha <volha.yurchanka@innowise.com>
2026-08-12 00:21:42 +0300

    add sensitive inputs and outputs

--- Udemy-course/Task-2/outputs.tf
+++ Udemy-course/Task-2/outputs.tf
@@ -1,14 +1,13 @@
 output application_name {
   value = var.application_name
 }
-
-output environment_name {
-  value = var.environment_name
-}
-
 output environment_prefix {
   value = local.environment_prefix
 }
 output "suffix" {
   value = random_string.suffix.result
+}
+output "api-key" {
+  value = "${var.api-key}"
+  sensitive = true
 }
--- Udemy-course/Task-2/variables.tf
+++ Udemy-course/Task-2/variables.tf
@@ -3,4 +3,7 @@ variable "application_name" {
 }
 variable "environment_name" {
 
-}  
+}
+variable "api-key" {
+    sensitive = true
+}
