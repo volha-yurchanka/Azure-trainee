cb03a1da910a721d2001a01f68ddb09a8c49ca99
Yurchanka-Volha <volha.yurchanka@innowise.com>
2026-08-12 18:32:00 +0300

    Create Storage Accunt

--- Udemy-course/Azure-tasks/main.tf
+++ Udemy-course/Azure-tasks/main.tf
@@ -1,4 +1,17 @@
-resource "azurerm_resource_group" "main" {
-  name     = "rg-foobar"
-  location = "westus3"
+data "azurerm_resource_group" "main" {
+  name = "rg-volha.yurchanka"
+}
+
+resource "random_string" "suffix" {
+  length = 10
+  upper = false
+  special = false
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "st${random_string.suffix.result}"
+  resource_group_name      = data.azurerm_resource_group.main.name
+  location                 = "westus3"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
--- Udemy-course/Azure-tasks/variables.tf
+++ Udemy-course/Azure-tasks/variables.tf
@@ -1,6 +1,12 @@
-variable applicatin_name {
-    type = string
+variable "application_name" {
+  type        = string
+  description = "Name of the application"
 }
-variable environment_name {
-    type = string
+variable "environment_name" {
+  type        = string
+  description = "Environment name (e.g. dev, prod)"
+}
+variable "primary_location" {
+  type        = string
+  description = "Primary Azure region"
 }
