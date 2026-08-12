3ce7a3201fe27efbd7f65ef5ef0abf585af4bd01
Yurchanka-Volha <volha.yurchanka@innowise.com>
2026-08-12 13:36:06 +0300

    Add Task-3 and type of variables in Task-2

--- Udemy-course/Task-2/env/env-prod.tfvars
+++ Udemy-course/Task-2/env/env-prod.tfvars
@@ -1,1 +1,9 @@
 environment_name = "prod"
+instance-count = 4
+enabled = false
+region = ["us-west-1", "us-east-1"]
+region_instance_count = {
+  "us-west-1" = 1
+  "us-east-1" = 3
+}
+region_set = ["us-west-1", "us-east-1"]
--- Udemy-course/Task-2/variables.tf
+++ Udemy-course/Task-2/variables.tf
@@ -1,9 +1,19 @@
 variable "application_name" {
-
+  type = string
 }
 variable "environment_name" {
-
+  type = string
 }
 variable "api-key" {
-    sensitive = true
+  type = string
+  sensitive = true
+}
+variable "instance-count" {
+  type = number
+}
+variable "region" {
+  type = list(string)
+}
+variable "region_instance_count" {
+  type = map(string)
 }
