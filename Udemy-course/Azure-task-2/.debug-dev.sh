79b97660af42501313ef55fed00a0344591e7851
Yurchanka-Volha <volha.yurchanka@innowise.com>
2026-08-18 00:54:58 +0300

    create local runner

---
+++ Udemy-course/Azure-task-2/.debug-dev.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# set the subscription
+export ARM_SUBSCRIPTION_ID="111-111-111-111-111"
+
+# set the application / environment
+export TF_VAR_application_name="observability"
+export TF_VAR_environment_name="dev"
+
+# set the backend 
+export BACKEND_RESOURCE_GROUP_NAME="rg-volha.yurchanka"
+export BACKEND_STORAGE_ACCOUNT_NAME="terraformtest8362"
+export BACKEND_CONTAINER_NAME="tfstate"
+export BACKEND_KEY=$TF_VAR_application_name-$TF_VAR_environment_name
+
+# run terraform init
+terraform init \
+  -backend-config="resource_group_name=${BACKEND_RESOURCE_GROUP_NAME}" \
+  -backend-config="storage_account_name=${BACKEND_STORAGE_ACCOUNT_NAME}" \
+  -backend-config="container_name=${BACKEND_CONTAINER_NAME}" \
+  -backend-config="key=${BACKEND_KEY}"
+
+terraform "$@"
+
+rm -rf .terraform
--- Udemy-course/Azure-task-2/versions.tf
+++ Udemy-course/Azure-task-2/versions.tf
@@ -6,10 +6,6 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "rg-volha.yurchanka"
-    storage_account_name = "terraformtest8362"
-    container_name       = "tfstate"
-    key                  = "observability-dev"
   }
 }
 
