49d503fbc5f412efd2887143f45d2ccff7af356c
Yurchanka-Volha <volha.yurchanka@innowise.com>
2026-08-17 21:56:17 +0300

    Try create Log Analytics

--- Udemy-course/Azure-task-2/main.tf
+++ Udemy-course/Azure-task-2/main.tf
@@ -1,3 +1,11 @@
 data "azurerm_resource_group" "main" {
   name = "rg-volha.yurchanka"
 }
+
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = "log-${var.application_name}-${var.environment_name}"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
